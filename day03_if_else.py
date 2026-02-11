883ef4ae4b760af049c3c77f18f2d178e66f2eaf
Srirangam Manobhiram <srirangammanobhiram@gmail.com>
2026-02-11 09:24:48 +0530

    Day 3 - if else practice programs

---
+++ day03_if_else.py
@@ -0,0 +1,27 @@
+# Day 03 IF ELSE Practice
+  # Even Or Odd Check
+num = int(input("Enter a number:"))
+if num % 2 == 0:
+    print("It is a Even Number")
+else:
+    print("It is a Odd Number")
+
+  # Largest of two numbers
+
+a = int(input("Enter First Number:"))
+b = int(input("Enter Second Number:"))
+if a>b :
+    print("a is larger")
+else:
+    print("b is larger")
+  # grade checker
+marks = int(input("Enter your marks:"))
+if marks>=90:
+    print("A grade")
+elif marks>=70:
+    print("B grade")
+elif marks>=50:
+    print("C grade")
+else:
+    print("Fail")
+          
