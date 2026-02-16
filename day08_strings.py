a9396be62f421dd6e27810bff65826a560152523
Srirangam Manobhiram <srirangammanobhiram@gmail.com>
2026-02-16 22:57:53 +0530

    Day 8 - learned string basics

---
+++ day08_strings.py
@@ -0,0 +1,18 @@
+# Day 8 - String basics
+
+name = "Python"
+
+# indexing
+print(name[0])
+print(name[-1])
+
+# length
+print("Length:", len(name))
+
+# concatenation
+first = "Hello"
+second = "World"
+print(first + " " + second)
+
+# repetition
+print("Code " * 3)
