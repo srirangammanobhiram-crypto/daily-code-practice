4177920a2df85944c8b6f276a4812e22b4ac0a5d
Srirangam Manobhiram <srirangammanobhiram@gmail.com>
2026-02-17 22:09:25 +0530

    Day 8 - string basics revision practice

---
+++ day08_strings_revision.py
@@ -0,0 +1,12 @@
+# Day 8 Revision - Strings
+
+name = input("Enter your name: ")
+
+print("First letter:", name[0])
+print("Last letter:", name[-1])
+print("Length:", len(name))
+
+greeting = "Hello "
+print(greeting + name)
+
+print("Welcome! " * 2)
