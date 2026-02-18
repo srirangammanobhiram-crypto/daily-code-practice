b82e966d924a1b48a4676930cc8365b2b08c050f
Srirangam Manobhiram <srirangammanobhiram@gmail.com>
2026-02-18 22:48:36 +0530

    Day 9 - learned string slicing

---
+++ day09_string_slicing.py
@@ -0,0 +1,11 @@
+# Day 9 - String slicing
+
+text = "PythonProgramming"
+
+print(text[0:6])     # Python
+print(text[6:])      # Programming
+print(text[:10])     # PythonProg
+print(text[-5:])     # mming
+
+# reverse string
+print(text[::-1])
