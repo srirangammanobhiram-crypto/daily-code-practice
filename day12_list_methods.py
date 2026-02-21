0aa872a37fa09374c2fae0810c1469dac58fd991
Srirangam Manobhiram <srirangammanobhiram@gmail.com>
2026-02-21 22:57:48 +0530

    Day 12 - learned list methods

---
+++ day12_list_methods.py
@@ -0,0 +1,23 @@
+# Day 12 - List methods
+
+numbers = [30, 10, 50]
+
+# add elements
+numbers.append(40)
+numbers.insert(1, 20)
+
+print("After adding:", numbers)
+
+# remove elements
+numbers.remove(10)
+numbers.pop()
+
+print("After removing:", numbers)
+
+# sort list
+numbers.sort()
+print("Sorted:", numbers)
+
+# reverse list
+numbers.reverse()
+print("Reversed:", numbers)
