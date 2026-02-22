10907bdc28cabf1e5fdc537e7168e76975ee636d
Srirangam Manobhiram <srirangammanobhiram@gmail.com>
2026-02-22 22:45:22 +0530

    Day 13 - learned tuple basics

---
+++ day13_tuples.py
@@ -0,0 +1,14 @@
+# Day 13 - Tuples basics
+
+numbers = (10, 20, 30, 40, 50)
+
+# access elements
+print(numbers[0])
+print(numbers[-1])
+
+# length
+print("Length:", len(numbers))
+
+# loop through tuple
+for num in numbers:
+    print("Value:", num)
